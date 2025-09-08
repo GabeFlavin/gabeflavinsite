6526dd3cab603da59d3e80eafbc2a48b172ceafe
Gabe <gabeflavin@gmail.com>
2025-09-08 09:32:38 -0400

    Refactor code structure for improved readability and maintainability

--- src/scan.js
+++ src/scan.js
@@ -2,6 +2,7 @@ import "https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js";
 import { Application } from 'https://unpkg.com/@splinetool/runtime@1.9.82/build/runtime.js';
 
 
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('/scene.splinecode');
+
+const canvasScan = document.getElementById('canvas3dScan');
+const scan = new Application(canvasScan);
+scan.load('/scene.splinecode2');
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        scan: resolve(__dirname, 'scan.html'),
+        '1': resolve(__dirname, '1.html'),
+        '2': resolve(__dirname, '2.html'),
+        '3': resolve(__dirname, '3.html'),
+        '4': resolve(__dirname, '4.html'),
+        '5': resolve(__dirname, '5.html'),
+        '6': resolve(__dirname, '6.html')
         //spline: resolve(__dirname, 'scene.splinecode'),
         //thorn: resolve(__dirname, 'thornfallsociety.html'),
         //fc3: resolve(__dirname, 'facecrusher3.html')
