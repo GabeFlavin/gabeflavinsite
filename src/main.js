587c84a0639a79defa1d26a01223bc653fc880c7
Gabe <gabeflavin@gmail.com>
2025-10-02 13:12:24 -0400

    Refactor services page layout and navigation integration
    - Updated services.html to improve structure and readability.
    - Added a navigation div (#con-nav) to load external navigation content.
    - Modified main.js to dynamically load the navigation HTML.
    - Adjusted CSS for #con-nav to ensure proper positioning and visibility.
    - Cleaned up vite.config.js to streamline page references and removed unused files.
    - Deleted obsolete vt.html file to reduce clutter.
    - Removed scene.splinecode3 as it is no longer needed.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
-import "https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js";
-// import { Application } from 'https://unpkg.com/@splinetool/runtime@1.9.82/build/runtime.js';
-import { Application } from '/runtime.js';
 
-
-const canvas = document.querySelector('#canvas3d') || document.createElement('canvas');
-canvas.id = 'canvas3d';
-const app = new Application(canvas);
-app.load('/scene.splinecode');
 
 // Function to load HTML includes
 async function loadInclude(selector, file) {
@@ -17,3 +9,33 @@ async function loadInclude(selector, file) {
 
 // Load navigation
 loadInclude('#con-nav', '/partials/nav.html');
+
+// import GSAP
+import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.12.2/index.js';
+
+import { Application } from 'https://unpkg.com/@splinetool/runtime@1.9.82/build/runtime.js';
+
+
+const canvas = document.querySelector('#canvas3d') || document.createElement('canvas');
+canvas.id = 'canvas3d';
+const app = new Application(canvas);
+app.load('/scene.splinecode');
+
+const canvas_contact = document.querySelector('#canvas3d_contact') || document.createElement('canvas');
+canvas_contact.id = 'canvas3d';
+const app_contact = new Application(canvas_contact);
+app_contact.load('/scene_contact.splinecode');
+
+// create a function that plays an audio file when called
+function playAudio(url) {
+  const audio = new Audio(url);
+  audio.play();
+}
+
+// Add event listener to play sound on canvas click check to see if canvas exists first
+if (canvas) {
+  canvas.addEventListener('click', () => {
+    playAudio('/sounds/click.mp3');
+  });
+}
+
